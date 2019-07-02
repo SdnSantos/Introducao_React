547baf292eb52819b027cd100546d234dc1e5eb0
Sidney Santos <sidney_ssantos@hotmail.com>
2019-07-01 23:27:50 -0300

    Modulo 04 - Estado e Imutabilidade

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 
 class TechList extends Component {
+  // todo estado no react é imutável, não podemos alterar ele diretamente
   state = {
+    newTech: "",
     techs: ["Node.js", "ReactJS", "React Native"]
+  };
+
+  // precisa ser no formato arrow function para ter acesso ao this
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // primeira posição copio todo o array com o spread
+    // segunda posição eu adiciono o novo elemento
+    // setState copia o estado, e aplica as alterações
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
   };
 
   render() {
     return (
-      <ul>
-        <li>{this.state.techs[0]}</li>
-        <li>{this.state.techs[1]}</li>
-        <li>{this.state.techs[2]}</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          // boa prática para caso o valor sofra alteraçao por qualquer
+          // outro motivo, o input atualizará também
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
