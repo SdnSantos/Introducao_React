fc4330c336874a53db433e258208db7bd8bb970f
Sidney Santos <sidney_ssantos@hotmail.com>
2019-07-01 23:54:58 -0300

    Modulo 04 - Propriedades do React

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TechItem from "./TechItem";
 
 class TechList extends Component {
   // todo estado no react é imutável, não podemos alterar ele diretamente
@@ -33,12 +34,11 @@ class TechList extends Component {
       <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button">
-                Remover
-              </button>
-            </li>
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
           ))}
         </ul>
         <input
