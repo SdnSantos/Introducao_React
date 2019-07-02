26edb2e11bf92c74787dc6cff2ca02424d17c977
Sidney Santos <sidney_ssantos@hotmail.com>
2019-07-01 21:34:37 -0300

    Modulo 04 - Importando CSS

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
   }
